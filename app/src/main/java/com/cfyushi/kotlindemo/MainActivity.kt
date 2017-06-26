67c4b33e29148691f95364f08b1d43f152752d85
wuzhishun <zhishun.wu@midea.com>
2017-06-26 10:45:12 +0800

    test

--- app/src/main/java/com/cfyushi/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/cfyushi/kotlindemo/MainActivity.kt
@@ -3,12 +3,14 @@ package com.cfyushi.kotlindemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        demo_intent.setOnClickListener {  }
+        demo_intent.setOnClickListener { info { "1111111111111111111234567" } }
     }
 }
